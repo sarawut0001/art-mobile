f8f94d8955946a7dd7d72a6fe8f536a919a3eabf
artzamukie001 <sarawut_boon@elearning.cmu.ac.th>
2025-03-15 12:41:18 +0700

    feat: connect api

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,10 +1,45 @@
 "use client";
 
 import { useState } from "react";
+import { config } from "../config";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { useRouter } from "next/navigation";
 
 export default function Signin() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const handleSignIn = async () => {
+    try {
+      const payload = { username, password };
+      const response = await axios.post(
+        `${config.apiUrl}/user/signin`,
+        payload
+      );
+
+      if (response.data.token !== null) {
+        localStorage.setItem("token", response.data.token);
+        router.push("/backoffice/dashboard");
+      } else {
+        Swal.fire({
+          title: "ตรวจสอบ User",
+          text: "ชื่อผู้ใช้งานหรือรหัสผ่านไม่ถูกต้อง",
+          icon: "warning",
+          timer: 2000,
+        });
+      }
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      Swal.fire({
+        title: "Error",
+        text: error.message,
+        icon: "error",
+      });
+    }
+  };
 
   return (
     <div className="signin-container">
@@ -19,12 +54,14 @@ export default function Signin() {
         />
         <div className="mt-4">Password</div>
         <input
-          type="text"
+          type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
 
-        <button className="mt-4">Sign In</button>
+        <button className="mt-4" onClick={handleSignIn}>
+          Sign In
+        </button>
       </div>
     </div>
   );
